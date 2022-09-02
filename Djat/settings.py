8852e4176722e4154bf85928c300d04e13580f0c
Ivan Matejcic <ivanmatejcic1@gmail.com>
2022-09-02 10:48:45 +0200

    Added Heroku app to trusted csrf origins

--- Djat/settings.py
+++ Djat/settings.py
@@ -149,3 +149,5 @@ DATABASES['default'].update(db_from_env)
 # Simplified static file serving.
 # https://pypi.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+CSRF_TRUSTED_ORIGINS = ["https://djat.herokuapp.com"]
