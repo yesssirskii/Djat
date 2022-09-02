8e86c1c5292078d16afc8f00007948e9b8ef832a
Ivan Matejcic <ivanmatejcic1@gmail.com>
2022-09-02 11:45:21 +0200

    Deployment modifications.

--- Djat/asgi.py
+++ Djat/asgi.py
@@ -1,13 +1,14 @@
 import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Djat.settings')
+django.setup()
 
 from django.core.asgi import get_asgi_application
-
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 
 import room.routing
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Djat.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
